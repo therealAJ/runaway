992def160726ee0d2846e2a8efd673c161ce2ed1
Anthony Liu <Anthony Liu>
2016-09-10 18:34:37 -0400

    implment browsing

--- pi/app.js
+++ pi/app.js
@@ -1,60 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require("express");
+let fs = require("fs");
+let serveIndex = require('serve-index');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+let app = express();
 
-var app = express();
+let ROOT = __dirname + '\\';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static(ROOT));
+app.use('/data', serveIndex(ROOT));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+/*
+ * Upload the file to the path
+ * Post body - file data
+ */
+app.post('/upload/:path', (req, res) => {
+  
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+app.listen(process.env.PORT || '3000');
+console.log("running on port " + (process.env.PORT || '3000'));
