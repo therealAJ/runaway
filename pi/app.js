2d6120d5c2f93f726b9e38057c7a0148ba4ec53f
Anthony Liu <Anthony Liu>
2016-09-10 19:06:04 -0400

    file uploading

--- pi/app.js
+++ pi/app.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let fs = require("fs");
 let serveIndex = require('serve-index');
+var busboy = require('connect-busboy');
 
 let app = express();
 
@@ -8,13 +9,22 @@ let ROOT = __dirname + '\\';
 
 app.use(express.static(ROOT));
 app.use('/data', serveIndex(ROOT));
+app.use(busboy());
 
 /*
  * Upload the file to the path
- * Post body - file data
  */
-app.post('/upload/:path', (req, res) => {
-  
+app.post('/fileupload', function(req, res) {
+  var fstream;
+  req.pipe(req.busboy);
+  req.busboy.on('file', function (fieldname, file, filename) {
+    console.log("Uploading: " + filename);
+    fstream = fs.createWriteStream(ROOT + filename);
+    file.pipe(fstream);
+    fstream.on('close', function () {
+      res.redirect('back');
+    });
+  });
 });
 
 app.listen(process.env.PORT || '3000');
